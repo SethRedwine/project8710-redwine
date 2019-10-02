4bbebd9f6869fa1d13f8717a2214542397ebbce2
Seth Redwine <sredwine@unomaha.edu>
2019-10-01 21:47:06 -0500

    New ex

---
+++ project-ex-1001-tree-redwine/src/model/Person.java
@@ -0,0 +1,55 @@
+/**
+ * @(#) Person.java
+ */
+package model;
+
+/**
+ * @since J2SE-1.8
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+	List<Person> list = new ArrayList<Person>();
+	Person parent = null;
+	String name = null;
+
+	public Person(String name) {
+		this.name = name;
+		parent = this;
+	}
+
+	public Person(String name, Person parent) {
+		this.name = name;
+		this.parent = parent;
+	}
+
+	public Person[] list() {
+		Person[] l = new Person[list.size()];
+		return list.toArray(l);
+	}
+
+	public void add(Person p) {
+		list.add(p);
+	}
+
+	public Person getParent() {
+		return this.parent;
+	}
+
+	public boolean hasChildren() {
+		return !this.list.isEmpty();
+	}
+
+	public Integer getNumChildren() {
+		Integer grandchildren = 0;
+		for (Person child : list) {
+			grandchildren += child.getNumChildren();
+		}
+		return this.list.size() + grandchildren;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
---
+++ project-ex-1001-tree-redwine/src/view/Viewer.java
@@ -0,0 +1,92 @@
+/**
+ * @(#) Viewer.java
+ */
+package view;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.swt.widgets.TreeItem;
+
+import model.ModelProvider;
+import model.Person;
+import view.provider.ViewPersonContentProvider;
+import view.provider.ViewPersonLabelProvider;
+
+/**
+ * @since J2SE-1.8
+ */
+public class Viewer {
+	private TreeViewer viewer;
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer.setContentProvider(new ViewPersonContentProvider());
+		viewer.setLabelProvider(new DelegatingStyledCellLabelProvider(new ViewPersonLabelProvider()));
+		List<Person> data = ModelProvider.INSTANCE.getPersons();
+		Person[] array = data.toArray(new Person[data.size()]);
+		viewer.setInput(array);
+		addListeners();
+	}
+
+	private void addListeners() {
+		Tree tree = (Tree) viewer.getControl();
+		tree.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				TreeItem item = (TreeItem) e.item;
+				if (item != null && item.getItemCount() > 0) {
+					item.setExpanded(!item.getExpanded());
+					viewer.refresh();
+				}
+			}
+		});
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+			@Override
+			public void doubleClick(DoubleClickEvent event) {
+				IStructuredSelection thisSelection = (IStructuredSelection) event.getSelection();
+				Object selectedNode = thisSelection.getFirstElement();
+				MessageDialog.openInformation(null, "Number of Child Nodes",
+						((Person) selectedNode).getNumChildren().toString());
+			}
+		});
+		tree = (Tree) viewer.getControl();
+		Listener listener = new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				TreeItem treeItem = (TreeItem) event.item;
+				final TreeColumn[] treeColumns = treeItem.getParent().getColumns();
+				viewer.getTree().getDisplay().asyncExec(new Runnable() {
+					@Override
+					public void run() {
+						for (TreeColumn treeColumn : treeColumns)
+							treeColumn.pack();
+					}
+				});
+			}
+		};
+		tree.addListener(SWT.Expand, listener);
+	}
+
+	@Focus
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+}
