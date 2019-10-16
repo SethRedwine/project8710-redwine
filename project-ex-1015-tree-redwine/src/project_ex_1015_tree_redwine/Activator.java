9c1b9835fe14c770c4363e85a6e960416d60db43
Seth Redwine <sredwine@unomaha.edu>
2019-10-15 20:58:16 -0500

    New ex

---
+++ project-ex-1015-tree-redwine/src/project_ex_1015_tree_redwine/Activator.java
@@ -0,0 +1,24 @@
+package project_ex_1015_tree_redwine;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	public static BundleContext getContext() {
+		return context;
+	}
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
